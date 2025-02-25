76096c47c55e185d87c38f93362f77d651529088
bear320 <aaabear320@gmail.com>
2025-02-25 19:55:39 +0800

    feat: 🎸 init

---
+++ chapter1/operations.py
@@ -0,0 +1,17 @@
+# addition
+print(10 + 5)
+
+# subtraction
+print(10 - 5)
+
+# multiplication
+print(10 * 5)
+
+# division
+print(10 / 5)
+
+# remainder
+print(10 % 5)
+
+# exponentiation
+print(10**5)
