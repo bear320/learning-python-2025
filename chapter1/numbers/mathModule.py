d56b0ca4c9105cf2f9716c6eef6f5949fb11616f
bear320 <aaabear320@gmail.com>
2025-02-26 20:40:55 +0800

    feat: 🎸 Functions for Numbers

---
+++ chapter1/numbers/mathModule.py
@@ -0,0 +1,16 @@
+import math
+
+# e
+print(math.e)
+
+# pi
+print(math.pi)
+
+# floor
+print(math.floor(4.99))  # 4
+
+# ceil
+print(math.ceil(4.01))  # 5
+
+# sqrt
+print(math.sqrt(16))  # 4.0
---
+++ chapter1/numbers/methods.py
@@ -0,0 +1,25 @@
+# absolute value
+print(abs(-10))  # 10
+print(abs(10))  # 10
+
+# string
+print(str(10))  # '10'
+
+# power
+print(pow(10, 4))  # 10000
+
+# maximum
+print(max(10, 20, 30, 40, 50))  # 50
+
+# minimum
+print(min(10, 20, 30, 40, 50))  # 10
+
+# round: for numbers ending in .5, Python rounds to the nearest even number
+print(round(2.5))  # 2
+print(round(3.5))  # 4
+
+# integer
+print(int(3.0))  # 3
+
+# float
+print(float(3))  # 3.0
