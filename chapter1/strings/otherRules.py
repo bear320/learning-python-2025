2d5d0ec6728818d6047c96960fefaae52742bbc3
bear320 <aaabear320@gmail.com>
2025-03-06 19:36:58 +0800

    feat: 🎸 String (other rules)

---
+++ chapter1/strings/otherRules.py
@@ -0,0 +1,9 @@
+# immutability
+name = "Sam Smith"
+name = "P" + name[1:]
+print(name)  # Pam Smith
+
+# string * numbers
+print("Oliver" * 5)  # OliverOliverOliverOliverOliver
+# print("Oliver" * "10")  # TypeError: can't multiply sequence by non-int of type 'str'
+# print("A" * 5.5)  # TypeError: can't multiply sequence by non-int of type 'float'
