42cb3c8d061711ed7d3cb90cc5d4c8ddfd3d2712
bear320 <aaabear320@gmail.com>
2025-03-27 22:09:07 +0800

    feat: 🎸 list methods (append, extend, pop, copy)

--- chapter1/list/methods.py
+++ chapter1/list/methods.py
@@ -31,3 +31,30 @@ print(friends)  # ['Alice', 'Wilson', 'Oliver']
 other_friends = ["Bob", "Charlie", "David"]
 other_friends = other_friends[::-1]
 print(other_friends)  # ['David', 'Charlie', 'Bob']
+
+
+# append()
+friends = ["Oliver", "Wilson", "Alice"]
+friends.append("Bob")
+print(friends)  # ['Oliver', 'Wilson', 'Alice', 'Bob']
+friends.append(10)
+print(friends)  # ['Oliver', 'Wilson', 'Alice', 'Bob', 10]
+
+# extend()
+friends = ["Oliver", "Wilson", "Alice"]
+other_friends = ["Bob", "Charlie", "David"]
+friends.extend(other_friends)
+print(friends)  # ['Oliver', 'Wilson', 'Alice', 'Bob', 'Charlie', 'David']
+
+# pop()
+friends = ["Oliver", "Wilson", "Alice"]
+lost_friend = friends.pop()
+print(lost_friend)  # Alice
+print(friends)  # ['Oliver', 'Wilson']
+
+# copy()
+friends = ["Oliver", "Wilson", "Alice"]
+other_friends = friends.copy()
+other_friends[1] = "Bob"
+print(friends)  # ['Oliver', 'Wilson', 'Alice']
+print(other_friends)  # ['Oliver', 'Bob', 'Alice']
