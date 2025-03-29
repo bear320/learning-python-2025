790ad7d4320d7df7d9ae48ad6ff445383de611ba
bear320 <aaabear320@gmail.com>
2025-03-29 19:02:12 +0800

    feat: 🎸 dict methods

---
+++ chapter1/dictionary/methods.py
@@ -0,0 +1,13 @@
+person = {
+    "name": "Oliver",
+    "age": 27,
+}
+
+# keys
+print(person.keys())  # dict_keys(['name', 'age'])
+
+# values
+print(person.values())  # dict_values(['Oliver', 27])
+
+# items
+print(person.items())  # dict_items([('name', 'Oliver'), ('age', 27)])
