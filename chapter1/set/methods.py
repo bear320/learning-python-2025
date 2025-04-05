4ded8d29fffba18b3a0f51c39de4ae32f9305648
bear320 <aaabear320@gmail.com>
2025-04-05 18:21:07 +0800

    feat: 🎸 Set built-in methods

--- chapter1/set/methods.py
+++ chapter1/set/methods.py
@@ -23,3 +23,60 @@ print(s)  # Output: {2, 3}
 # clear()
 s.clear()
 print(s)  # Output: set() (empty set)
+
+# difference()
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+
+diff = s1.difference(s2)
+print(diff)  # Output: {1, 2}
+
+diff = s2.difference(s1)
+print(diff)  # Output: {4, 5}
+
+# intersection()
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+
+inter = s1.intersection(s2)
+print(inter)  # Output: {3}
+
+inter = s2.intersection(s1)
+print(inter)  # Output: {3}
+
+# union()
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+
+union = s1.union(s2)
+print(union)  # Output: {1, 2, 3, 4, 5}
+
+union = s2.union(s1)
+print(union)  # Output: {1, 2, 3, 4, 5}
+
+# isdisjoint()
+s1 = {1, 2, 3}
+s2 = {4, 5, 6}
+
+is_disjoint = s1.isdisjoint(s2)
+print(is_disjoint)  # Output: True
+
+# issubset()
+s1 = {1, 2, 3}
+s2 = {1, 2, 3, 4, 5}
+
+is_subset = s1.issubset(s2)
+print(is_subset)  # Output: True
+
+is_subset = s2.issubset(s1)
+print(is_subset)  # Output: False
+
+# issuperset()
+s1 = {1, 2, 3}
+s2 = {1, 2, 3, 4, 5}
+
+is_superset = s1.issuperset(s2)
+print(is_superset)  # Output: False
+
+is_superset = s2.issuperset(s1)
+print(is_superset)  # Output: True
