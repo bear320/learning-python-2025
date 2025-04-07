44e8d4ddeb5732e9d2f7daffca9397d364000ce6
bear320 <aaabear320@gmail.com>
2025-04-07 21:52:18 +0800

    feat: 🎸 Type checking

---
+++ chapter1/typeChecking.py
@@ -0,0 +1,14 @@
+name = "Oliver"
+print(type(name))  # Output: <class 'str'>
+
+age = 27
+print(type(age))  # Output: <class 'int'>
+
+height = 5.9
+print(type(height))  # Output: <class 'float'>
+
+is_married = False
+print(type(is_married))  # Output: <class 'bool'>
+
+family = ["Alice", "Bob", "Charlie"]
+print(type(family))  # Output: <class 'list'>
