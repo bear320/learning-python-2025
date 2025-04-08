88c8e07978046e1e06f93a08335c6445ffbc0064
bear320 <aaabear320@gmail.com>
2025-04-08 22:08:46 +0800

    feat: 🎸 Additional infomation part 1

---
+++ chapter1/additional.py
@@ -0,0 +1,17 @@
+# Python supports complex numbers with a real and imaginary part.
+# The imaginary part is denoted with a j.
+x = 3 + 4j
+y = 2 + 5j
+print(x)  # (3+4j)
+print(y)  # (2+5j)
+print(x + y)  # (5+9j)
+print(x - y)  # (1-1j)
+
+
+# None
+def hello():  # hello is a function stored in RAM
+    print("Hello, World!")
+
+
+print(hello)  # <function hello at 0x7f8c1c3e2d30>
+print(hello())  # Hello, World! # None
