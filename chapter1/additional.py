1bdf885cb508c0a859a399d47e096d1932bb0069
bear320 <aaabear320@gmail.com>
2025-04-12 23:04:25 +0800

    feat: 🎸 Additional infomation

--- chapter1/additional.py
+++ chapter1/additional.py
@@ -15,3 +15,13 @@ def hello():  # hello is a function stored in RAM
 
 print(hello)  # <function hello at 0x7f8c1c3e2d30>
 print(hello())  # Hello, World! # None
+
+# floating-point binary problem
+print(0.1 + 0.2 - 0.3)  # 5.551115123125783e-17
+
+# join()
+my_list_1 = ["Hello", "World"]
+print(" ".join(my_list_1))  # Hello World
+
+my_list_2 = ["a", "b", "c"]
+print(", ".join(my_list_2))  # a, b, c
