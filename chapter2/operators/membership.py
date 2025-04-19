ed9526c4813ec4dc5c51e10d038258c1603dd913
bear320 <aaabear320@gmail.com>
2025-04-19 23:31:47 +0800

    feat: 🎸 Membership operator

---
+++ chapter2/operators/membership.py
@@ -0,0 +1,17 @@
+# string membership
+my_string = "ABCD"
+
+if "A" in my_string:
+    print("A is in", my_string)
+
+# set membership
+my_set = {1, 2, 3, 4}
+
+if 1 in my_set:
+    print("1 is in", my_set)
+
+# list membership
+my_list = [1, 2, 3, 4]
+
+if 1 in my_list:
+    print("1 is in", my_list)
