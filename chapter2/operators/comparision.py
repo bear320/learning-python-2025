bdcbfd76ff5b749db27955a2d5c703871fde64a3
bear320 <aaabear320@gmail.com>
2025-04-20 16:06:10 +0800

    feat: 🎸 Comparison operators

---
+++ chapter2/operators/comparision.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+print(a == b)  # False
+print(a != b)  # True
+print(a < b)  # True
+print(a > b)  # False
+print(a <= b)  # True
+print(a >= b)  # False
+
+msg = "Today is a good day"
+print(msg == msg.lower())  # False
+print(msg != msg.lower())  # True
