73c886f1513df1a39771405b410dcbe610a27d62
bear320 <aaabear320@gmail.com>
2025-05-04 20:10:39 +0800

    feat: 🎸 Bitwise operators

---
+++ chapter2/operators/bitwise.py
@@ -0,0 +1,23 @@
+a = 60
+b = 13
+
+# Bitwise AND
+print(a & b)  # 12
+
+# Bitwise OR
+print(a | b)  # 61
+
+# Bitwise XOR
+print(a ^ b)  # 49
+
+# Bitwise NOT
+print(~a)  # -61
+print(~b)  # -14
+
+# Left Shift
+print(a << 2)  # 240
+print(b << 2)  # 52
+
+# Right Shift
+print(a >> 2)  # 15
+print(b >> 2)  # 3
