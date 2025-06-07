cc99544ab607ff407ed18dc5d43a5b65f46d37a9
bear320 <aaabear320@gmail.com>
2025-06-07 21:31:49 +0800

    feat: 🎸 for loop

---
+++ chapter3/for_loop.py
@@ -0,0 +1,25 @@
+# string
+for letter in "Hello World":
+    print(letter.upper())
+
+# list
+my_list = [1, 2, 3, 4, 5]
+
+for num in my_list:
+    print(num)
+
+# tuple list
+for a, b in [(1, 2), (3, 4), (5, 6)]:
+    print(a, b)
+
+# dictionary
+my_dict = {"name": "Alice", "age": 30}
+
+for key, value in my_dict.items():
+    print(f"{key}: {value}")
+
+# set
+my_set = {1, 2, 3, 4, 5}
+
+for num in my_set:
+    print(num)
