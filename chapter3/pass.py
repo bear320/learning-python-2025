324dbc5d1ad36d9703ef586709a6e9fa0ad8532e
bear320 <aaabear320@gmail.com>
2025-06-15 19:55:08 +0800

    feat: 🎸 pass

---
+++ chapter3/pass.py
@@ -0,0 +1,19 @@
+# loop
+for i in "How are you?":
+    pass
+
+print("This loop does nothing.")
+
+# control flow
+if True:
+    pass
+else:
+    print("This won't be printed.")
+
+
+# function
+def do_nothing():
+    pass
+
+
+do_nothing()
