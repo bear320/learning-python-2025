b6d71eaac4c4dde2c8c9ae31805158c962df4011
bear320 <aaabear320@gmail.com>
2025-07-05 22:00:34 +0800

    feat: 🎸 generator comprehension

--- chapter3/comprehension.py
+++ chapter3/comprehension.py
@@ -21,4 +21,12 @@ print(squared_x)  # {3: 9, 4: 16, 5: 25}
 # set comprehension
 squared_x = {i**2 for i in x if i > 2}
 
-print(squared_x)  # {9, 16, 25}
+print(squared_x)  # {16, 9, 25}
+
+# generator comprehension
+squared_x = (i**2 for i in x if i > 2)
+
+print(squared_x)
+
+for i in squared_x:
+    print(i)  # 9, 16, 25
