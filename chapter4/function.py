0b3d70f301256ba4893e0647cea6867149ba5bac
bear320 <aaabear320@gmail.com>
2025-07-10 23:15:23 +0800

    feat: 🎸 paramenters vs. arguments

---
+++ chapter4/function.py
@@ -0,0 +1,20 @@
+def say_hi():
+    print("Hi there! How are you?")
+
+
+# function execution / invocation
+say_hi()
+
+
+def add(x, y):  # x and y are parameters
+    """
+    This function takes two numbers and returns their sum.
+
+    :param x: First number
+    :param y: Second number
+    :return: Sum of x and y
+    """
+    return x + y
+
+
+print(add(1, 2))  # 1 and 2 are arguments
