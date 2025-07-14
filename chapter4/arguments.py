2ec1d19c1127289fa18d9773fdf9917d39d7ae0f
bear320 <aaabear320@gmail.com>
2025-07-14 22:44:45 +0800

    feat: 🎸 default arguments

---
+++ chapter4/arguments.py
@@ -0,0 +1,26 @@
+def exponent(a, b):
+    return a**b
+
+
+# positional arguments
+print(exponent(2, 3))  # 8
+
+# keyword arguments
+print(exponent(b=2, a=3))  # 9
+
+
+# default arguments
+def sum(n1=0, n2=0):
+    return n1 + n2
+
+
+# SyntaxError: parameter without a default follows parameter with a default
+# def sum2(n1=10, n2):
+#     return n1 + n2
+
+print(sum())  # 0
+print(sum(12))  # 12
+print(sum(12, 8))  # 20
+
+print(sum(n2=5))  # 5
+print(sum(n2=10, n1=5))  # 15
--- chapter4/positional_and_keyword_arguments.py
+++
@@ -1,9 +0,0 @@
-def exponent(a, b):
-    return a**b
-
-
-# positional arguments
-print(exponent(2, 3))  # 8
-
-# keyword arguments
-print(exponent(b=2, a=3))  # 9
