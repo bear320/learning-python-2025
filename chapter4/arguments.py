ea44e3b082c5425b8f13ba8524e6e1d628a48965
bear320 <aaabear320@gmail.com>
2025-07-15 23:20:23 +0800

    feat: 🎸 arbitrary number of arguments (*args)

--- chapter4/arguments.py
+++ chapter4/arguments.py
@@ -24,3 +24,16 @@ print(sum(12, 8))  # 20
 
 print(sum(n2=5))  # 5
 print(sum(n2=10, n1=5))  # 15
+
+
+# arbitrary arguments
+def sum_all(*args):
+    total = 0
+    for n in range(len(args)):
+        total += args[n]
+        print(f"Adding {args[n]} to total, current total: {total}")
+    return total
+
+
+print(sum_all(1, 2, 3))  # 6
+print(sum_all(2, -3, 4, 5))  # 8
