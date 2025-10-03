4b70915324a7ebea3b07be6bce7459228ae8ffbd
bear320 <aaabear320@gmail.com>
2025-10-03 20:03:34 +0800

    feat: 🎸 simple exercise i - exercise 2

--- practices/simple_exercise_i.py
+++ practices/simple_exercise_i.py
@@ -1,9 +1,18 @@
 # 1. Write a function called "printMany" that prints out integers 1, 2, 3, ..., 100.
-
-
 def printMany():
     for i in range(1, 101):
         print(i)
+    print("Example complete!")
 
 
 printMany()
+
+
+#  2. Write a function called "printEvery3" that prints out integers 1, 4, 7, 10, ..., 88.
+def printEvery3():
+    for i in range(1, 89, 3):
+        print(i)
+    print("Example complete!")
+
+
+printEvery3()
