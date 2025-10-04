412349e69a9a5d911ab786b54b1c23a1ad5be80a
bear320 <aaabear320@gmail.com>
2025-10-04 23:20:47 +0800

    feat: 🎸 simple exercise i - exercise 3

--- practices/simple_exercise_i.py
+++ practices/simple_exercise_i.py
@@ -2,17 +2,28 @@
 def printMany():
     for i in range(1, 101):
         print(i)
-    print("Example complete!")
 
 
-printMany()
+# printMany()
 
 
 #  2. Write a function called "printEvery3" that prints out integers 1, 4, 7, 10, ..., 88.
 def printEvery3():
     for i in range(1, 89, 3):
         print(i)
-    print("Example complete!")
 
 
-printEvery3()
+# printEvery3()
+
+
+#  3. Write a function called "position" that returns a tuple of the first uppercase letter and its index location. If not found, returns -1.
+def position(string):
+    for i, char in enumerate(string):
+        if char.isupper():
+            return (char, i)
+    return -1
+
+
+print(position("abcd"))
+print(position("AbcD"))
+print(position("abCD"))
