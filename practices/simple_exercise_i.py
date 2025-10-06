8f0774758a8dee5ccbf63604ba1a9803ce69a7a9
bear320 <aaabear320@gmail.com>
2025-10-06 17:21:55 +0800

    feat: 🎸 simple exercise i - exercise 6 & 7

--- practices/simple_exercise_i.py
+++ practices/simple_exercise_i.py
@@ -51,7 +51,34 @@ def findSmallerTotal(list, int):
     return total
 
 
-print(findSmallerTotal([1, 2, 3], 3))  # returns 3
-print(findSmallerTotal([1, 2, 3], 1))  # returns 0
-print(findSmallerTotal([3, 2, 5, 8, 7], 999))  # returns 25
-print(findSmallerTotal([3, 2, 5, 8, 7], 0))  # returns 0
+# print(findSmallerTotal([1, 2, 3], 3))  # returns 3
+# print(findSmallerTotal([1, 2, 3], 1))  # returns 0
+# print(findSmallerTotal([3, 2, 5, 8, 7], 999))  # returns 25
+# print(findSmallerTotal([3, 2, 5, 8, 7], 0))  # returns 0
+
+
+# 6. Write a function called "findAllSmall" that takes one list of integers and another integer as input, and returns an list of integers that contains all elements that are smaller than the input integer.
+def findAllSmall(list, int):
+    smallerInts = []
+    for i in list:
+        if i < int:
+            smallerInts.append(i)
+    return smallerInts
+
+
+# print(findAllSmall([1, 2, 3], 10))  # returns [1, 2, 3]
+# print(findAllSmall([1, 2, 3], 2))  # returns [1]
+# print(findAllSmall([1, 3, 5, 4, 2], 4))  #  returns [1, 3, 2]
+
+
+# 7. Write a function called "summ" that takes one list of numbers, and return the sum of all elements in the input list.
+def summ(list):
+    sum = 0
+    for i in list:
+        sum += i
+    return sum
+
+
+print(summ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  # returns 55
+print(summ([]))  # return 0
+print(summ([-10, -20, -30]))  # return -60
