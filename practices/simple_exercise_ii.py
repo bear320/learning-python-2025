bb5d6a9b9e43bc32792e66f7c866bf2302d50735
bear320 <aaabear320@gmail.com>
2025-10-11 19:33:45 +0800

    feat: 🎸 simple exercise ii - exercise 3 & 4

--- practices/simple_exercise_ii.py
+++ practices/simple_exercise_ii.py
@@ -40,7 +40,46 @@ def stars2(n):
         print("*" * j)
 
 
-stars2(1)
-stars2(2)
-stars2(3)
-stars2(4)
+# stars2(1)
+# stars2(2)
+# stars2(3)
+# stars2(4)
+
+
+# 3. Write a function called "table" which takes an input n, and prints out n x 1 to n x 9
+
+# table(3);
+# 3 x 1 = 3
+# 3 x 2 = 6
+# ...
+# 3 x 9 = 27
+
+
+def table(n):
+    for i in range(1, 10):
+        print(f"{n} x {i} = {n*i}")
+
+
+# table(3)
+
+# 4. Write a function called "table9to9" that prints out the multiplication table:
+
+# table9to9();
+# 1 x 1 = 1
+# 1 x 2 = 2
+# 1 x 3 = 3
+# ...
+# 1 x 9 = 9
+# 2 x 1 = 2
+# 2 x 2 = 4
+# ...
+# 9 x 9 = 81
+
+
+def table9to9():
+    for i in range(1, 10):
+        for j in range(1, 10):
+            print(f"{i} x {j} = {i*j}")
+
+
+table9to9()
