c2409cc347dd2ebc528986d7011b1e66fff60154
bear320 <aaabear320@gmail.com>
2025-11-04 15:54:13 +0800

    feat: 🎸 readline, readlines & close

--- chapter5/files.py
+++ chapter5/files.py
@@ -2,9 +2,22 @@ file = open("chapter5/my_file.txt", "r")
 
 print(file.read(5))
 print(file.read(3))
-file.seek(0)
-print(file.read())
 
+file.seek(0)
+
+print(file.read())  # returns a string
 print(type(file.read()))  # <class 'str'>
+
+file.seek(0)
+
+print(file.readlines())  # returns a list of strings
+
+# for line in file.readlines():
+#     print(line)
+
+file.seek(0)
+
+print(file.readline())  # returns the first line
+print(file.readline())  # returns the second line
 
 file.close()
