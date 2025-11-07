3e698085666b2c99c6c580c833d162d6ba726fef
bear320 <aaabear320@gmail.com>
2025-11-07 22:36:58 +0800

    feat: 🎸 user input

---
+++ chapter5/user_input.py
@@ -0,0 +1,7 @@
+user_input = input("How old are you?")
+
+print("--- Start of Output ---")
+print(user_input)
+print(type(user_input))
+print("You are " + user_input + " years old.")
+print("--- End of Output ---")
