893e1a1a9ae9b3b3e854c76e14fce9aeb2536350
bear320 <aaabear320@gmail.com>
2025-11-18 20:17:20 +0800

    feat: 🎸 static and class method

--- chapter6/class.py
+++ chapter6/class.py
@@ -21,6 +21,11 @@ class Animal:
     def greet(self):
         print(f"Hello, I am {self.name} and belong to {self.__class__.farm}.")
 
+    # static method
+    @staticmethod
+    def introduce():
+        print("Welcome to the Animal Farm!")
+
 
 Usagi = Animal("Usagi", "Rabbit", 3)
 # print(type(Usagi))  # Output: <class '__main__.Animal'>
@@ -42,7 +47,44 @@ Momonga = Animal("Momonga", "Flying Squirrel", 2)
 
 # print(Usagi.age > Momonga.age)  # Output: True
 
-print(Animal.farm)  # Output: Chiikawa World
-print(Usagi.farm)  # Output: Chiikawa World
+# print(Animal.farm)  # Output: Chiikawa World
+# print(Usagi.farm)  # Output: Chiikawa World
 
-Usagi.greet()  # Output: Hello, I am Usagi and belong to Chiikawa World.
+# Usagi.greet()  # Output: Hello, I am Usagi and belong to Chiikawa World.
+
+Animal.introduce()  # Output: Welcome to the Animal Farm!
+Usagi.__class__.introduce()  # Output: Welcome to the Animal Farm!
+
+
+class Circle:
+    """A class representing a circle with a given radius."""
+
+    pi = 3.14159
+    all_circles = []
+
+    def __init__(self, radius):
+        self.radius = radius
+        self.__class__.all_circles.append(self)
+
+    def area(self):
+        return self.__class__.pi * (self.radius**2)
+
+    @staticmethod
+    def total_area():
+        total = 0
+        for circle in Circle.all_circles:
+            total += circle.area()
+        return total
+
+    @classmethod
+    def total_area_cls(cls):
+        total = 0
+        for circle in cls.all_circles:
+            total += circle.area()
+        return total
+
+
+c1 = Circle(3)
+c2 = Circle(4)
+print(c1.__class__.total_area())
+print(c1.__class__.total_area_cls())
